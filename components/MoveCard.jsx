46586d9a929b4d0b7b3472e75ce37bdfc57864e5
aayush <aausgh@gmail.com>
2023-12-28 21:38:41 +0545

    fix: removed import

--- components/MoveCard.jsx
+++ components/MoveCard.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { typeColors } from '@utilities/Colors';
-import PopOver from './PopOver';
 
 const MoveCard = ({ move }) => {
       const [moveDetails, setMoveDetails] = useState(null);
