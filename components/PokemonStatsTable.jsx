af33f5b883ad7838c4ae3f1da8e8bc8af7f6b3a6
aayush <aausgh@gmail.com>
2024-01-03 15:51:39 +0545

    fix: removed unwanted codes

--- components/PokemonStatsTable.jsx
+++ components/PokemonStatsTable.jsx
@@ -1,21 +1,8 @@
 import React from 'react';
 
 const PokemonStatsTable = ({ stats }) => {
-      if (!stats || !stats.map) {
-            // Add a check for undefined or non-array stats
-            return null;
-      }
 
-      const calculatePercentage = (value, maxValue) => {
-            return (value / maxValue) * 100;
-      };
 
-      const getMaxStatValue = () => {
-            // Assuming max stat value is 150, adjust as needed
-            return Math.max(...stats.map((stat) => stat.base_stat), 255);
-      };
-
-      const maxStatValue = getMaxStatValue();
 
       return (
             <div className="py-8">
@@ -28,24 +15,34 @@ const PokemonStatsTable = ({ stats }) => {
                               </tr>
                         </thead>
                         <tbody >
-                              {stats.map((stat, index) => (
-                                    <tr key={index} className='odd:bg-gray-600 even:bg-gray-800'>
-                                          <td className="py-4 px-8 text-white text-lg capitalize ">{stat.stat.name}</td>
-                                          <td className="py-2 px-8">
-                                                <div className='w-full bg-gray-200 rounded-full dark:bg-gray-700'>
-                                                      <div
-                                                            className="bg-blue-600 text-xs font-medium text-black text-center p-2 leading-none rounded-full"
-                                                            style={{
-                                                                  width: `${calculatePercentage(stat.base_stat, maxStatValue)}%`,
-                                                                  backgroundColor: '#30a6d6',
-                                                            }}
-                                                      >
-                                                            {stat.base_stat}
+                              {stats.map((stat, index) => {
+
+                                    const value = stat.base_stat
+                                    const percentage = (value / 200) * 100;
+                                    return (
+
+                                          <tr key={index} className='odd:bg-gray-600 even:bg-gray-800'>
+
+                                                <td className="py-4 px-8 text-white text-lg capitalize ">{stat.stat.name}</td>
+
+                                                <td className="py-2 px-8">
+
+                                                      <div className="w-full bg-gray-200 rounded-full dark:bg-gray-700">
+
+                                                            <div
+                                                                  className="bg-blue-600 text-xs font-medium text-blue-100 text-center py-1 leading-none rounded-full"
+                                                                  style={{
+                                                                        width: `${percentage}%`,
+                                                                  }}>
+                                                                  {stat.base_stat}
+                                                            </div>
+
                                                       </div>
-                                                </div>
-                                          </td>
-                                    </tr>
-                              ))}
+
+                                                </td>
+                                          </tr>
+                                    )
+                              })}
 
                         </tbody>
                   </table>
